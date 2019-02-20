8d141840da6034b30ce4439f70d6aca33f411532
Joseph Divalentone <jmd16a@lib-301-10-9117.local>
2019-02-20 08:50:18 -0600

    Almost theree

--- FoodTrackerJMD/ViewController.swift
+++ FoodTrackerJMD/ViewController.swift
@@ -8,25 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     //MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
-    
     @IBOutlet weak var mealNameLabel: UILabel!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text fields user input through delegate call backs
+        nameTextField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       // hide the key board
+        textField.resignFirstResponder()
+        return true
     }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: UIImagePickerControlDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        //info dict may have multiple representations of the image
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+            else {
+                fatalError("expected a dict but was provided the following: \(info)")
+        }
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
     //MARK:Actions
     
-    @IBAction func setDefaultLabelText(_ sender: Any) {
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        //lets user pick pic
+        let imagePickerController = UIImagePickerController()
+        //only photos picked
+        imagePickerController.sourceType = .photoLibrary
+        // Veiw controller is notified
+        imagePickerController.delegate = self
+        present(imagePickerController,animated: true, completion: nil)
+        
+    }
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
     }
 }
 
